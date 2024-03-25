5d24106d3830f3032e40a83a54fd1bfed198e2dc
CedricSanchezGithub <mycedmail@gmail.com>
2024-03-25 13:18:20 +0100

    commit

--- src/main/kotlin/org/mymatchback/apirest/MatchAPI.kt
+++ src/main/kotlin/org/mymatchback/apirest/MatchAPI.kt
@@ -2,6 +2,7 @@ package org.mymatchback.apirest
 
 import org.mymatchback.model.MatchBean
 import org.mymatchback.model.MatchService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -29,10 +30,20 @@ class MymatchAPI(val matchService: MatchService){
     //  Une url pour faire évoluer le score du matche ou le déclarer fini
     @PostMapping("/score")
     fun add1Point(@RequestBody match: MatchBean,equipe:String){
+        println("/score : ${matchService.add1Point(match, equipe)}")
         matchService.add1Point(match, equipe)
-
     }
     //   Une url pour récupérer la liste des matches des 7 derniers jours avec les plus récents en 1er
+
+    @GetMapping("/7dayz")
+    fun lastSevenDayz() : List<MatchBean?>{
+
+
+       matchService.returnLastSevenDays(tabMatch)
+
+        return tabMatch
+    }
+
 
 }
 
--- src/main/kotlin/org/mymatchback/model/Beans.kt
+++
@@ -1,4 +0,0 @@
-package org.mymatchback.model
-
-import java.time.LocalDate
-
