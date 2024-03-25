9ea125aad506d1d1fef21f7eed525dc43170f5ee
CedricSanchezGithub <mycedmail@gmail.com>
2024-03-25 16:01:01 +0100

    25/03

--- src/main/kotlin/org/mymatchback/apirest/MatchAPI.kt
+++ src/main/kotlin/org/mymatchback/apirest/MatchAPI.kt
@@ -6,15 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDate
+
+        const val SEVENDAYZ = 604_800_000
 
 @RestController
 @RequestMapping("/mymatch")
 class MymatchAPI(val matchService: MatchService){
 
-    var tabMatch = List<MatchBean?>(10) {null}
+    @GetMapping("/test")
+    fun testHello(){
+        println("hello la team")
+    }
 
     // Une url pour créer un matche en donnant les 2 équipes et la date
 
@@ -23,27 +26,25 @@ class MymatchAPI(val matchService: MatchService){
     fun createMatch(@RequestBody match: MatchBean){
 
         println("/save match = $match")
-        matchService.createMatch(equipe1 = match.equipe1, equipe2 = match.equipe2, date = match.date)
+        match.date?.let { matchService.createMatch(equipe1 = match.equipe1, equipe2 = match.equipe2, date = it) }
 
     }
 
     //  Une url pour faire évoluer le score du matche ou le déclarer fini
     @PostMapping("/score")
-    fun add1Point(@RequestBody match: MatchBean,equipe:String){
+    fun add1Point(@RequestBody match: MatchBean,equipe:Int){
         println("/score : ${matchService.add1Point(match, equipe)}")
         matchService.add1Point(match, equipe)
     }
     //   Une url pour récupérer la liste des matches des 7 derniers jours avec les plus récents en 1er
 
+
+
     @GetMapping("/7dayz")
-    fun lastSevenDayz() : List<MatchBean?>{
-
-
-       matchService.returnLastSevenDays(tabMatch)
-
-        return tabMatch
+    fun lastSevenDayz() : List<MatchBean>{
+        val THISDAY = System.currentTimeMillis()
+        val daySearch = THISDAY - SEVENDAYZ
+        return matchService.last7Dayz(date = daySearch)
     }
-
-
 }
 
--- src/main/kotlin/org/mymatchback/model/Match.kt
+++ src/main/kotlin/org/mymatchback/model/Match.kt
@@ -4,26 +4,6 @@ import jakarta.persistence.*
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
-import java.time.LocalDate
-
-fun main() {
-    val matchs = listOf(
-        MatchBean(date = LocalDate.of(2024, 3, 20), equipe1 = "Nantes", equipe2 = "MHB", score_equipe1 = 22, score_equipe2 = 30, status = false),
-        MatchBean(date = LocalDate.of(2024, 3, 21), equipe1 = "MHB", equipe2 = "Nantes", score_equipe1 = 25, score_equipe2 = 28, status = false),
-        MatchBean(date = LocalDate.of(2024, 3, 19), equipe1 = "HBLM", equipe2 = "MHB", score_equipe1 = 21, score_equipe2 = 29, status = false),
-        MatchBean(date = LocalDate.of(2024, 3, 18), equipe1 = "Poitier", equipe2 = "MHB", score_equipe1 = 24, score_equipe2 = 28, status = false),
-        MatchBean(date = LocalDate.of(2024, 3, 20), equipe1 = "HBLM", equipe2 = "MHB", score_equipe1 = 23, score_equipe2 = 27, status = false),
-        MatchBean(date = LocalDate.of(2024, 3, 19), equipe1 = "Poitier", equipe2 = "MHB", score_equipe1 = 24, score_equipe2 = 23, status = false),
-        MatchBean(date = LocalDate.of(2024, 3, 21), equipe1 = "Poitier", equipe2 = "PSG", score_equipe1 = 27, score_equipe2 = 22, status = false)
-    )
-
-    // Affichage des matchs pour vérification
-    matchs.forEach { match ->
-        println("${match.equipe1} vs ${match.equipe2} le ${match.date}: ${match.score_equipe1}-${match.score_equipe2}, Status: ${match.status}")
-    }
-
-
-}
 
 
 
@@ -34,7 +14,7 @@ data class MatchBean(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id:Long? = null,
-    var date: LocalDate = LocalDate.of(1988, 1,1),
+    var date: Long? = null,
     var equipe1: String = "Equipe 1",
     var equipe2: String = "Equipe 2",
     var score_equipe1: Int = 0,
@@ -42,12 +22,17 @@ data class MatchBean(
     var status: Boolean = false)
 
 @Repository
-interface MatchRepository : JpaRepository<MatchBean, Long>
+interface MatchRepository : JpaRepository<MatchBean, Long>{
+
+    fun findAllByDateAfterOrderByDateDesc(date: Long):  List<MatchBean>
+
+}
 
 @Service
 class MatchService(val matchRep:MatchRepository) {
 
-    fun createMatch(equipe1:String?, equipe2:String?, date: LocalDate) : MatchBean {
+    //Créer un match avec nom de la première équipe, nom de la seconde équipe, la date du jour, initialisé à 0 - 0
+    fun createMatch(equipe1:String?, equipe2:String?, date: Long) : MatchBean {
         if(equipe1.isNullOrBlank()){
             throw Exception("equipe 1 missing")
         }
@@ -55,33 +40,27 @@ class MatchService(val matchRep:MatchRepository) {
             throw Exception("equipe 2 missing")
         }
 
-        val match = MatchBean(null,date, equipe1, equipe2, 0, 0)
+        val match = MatchBean(null, date, equipe1, equipe2, 0, 0)
+
         matchRep.save(match)
+
         return match
     }
-    fun add1Point(match: MatchBean, equipe:String):MatchBean{
+
+    //Incrémente 1 point à l'équipe
+    fun add1Point(match: MatchBean, equipe:Int):MatchBean{
         if(!match.status){
             throw Exception("Le match est finis")
         }else{
-            if (equipe == match.equipe1){
-                match.score_equipe1++
-            }else{
-                match.score_equipe2++
-            }
+            if (equipe == 1) match.score_equipe1++ else match.score_equipe2++
         }
         matchRep.save(match)
         return match
     }
 
-    fun returnLastSevenDays(matchs: List<MatchBean?>): List<MatchBean> {
-        val ilYaSeptJours = LocalDate.now().minusDays(7)
 
-        val matchsRecents = matchs.filterNotNull().filter { match ->
-            match.date.isAfter(ilYaSeptJours) || match.date.isEqual(ilYaSeptJours)
-        }
-        println(matchsRecents)
-        return matchsRecents
-    }
+    //Retourne les 7 derniers matchs trié par date
+    fun last7Dayz(date: Long) = matchRep.findAllByDateAfterOrderByDateDesc(date = date)
 
 }
 
