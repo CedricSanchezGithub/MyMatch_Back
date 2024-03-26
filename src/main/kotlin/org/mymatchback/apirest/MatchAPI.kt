aea8021259c081319700b72b133f68a3d3e94638
CedricSanchezGithub <mycedmail@gmail.com>
2024-03-26 15:53:49 +0100

    sendget

--- src/main/kotlin/org/mymatchback/apirest/MatchAPI.kt
+++ src/main/kotlin/org/mymatchback/apirest/MatchAPI.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-        const val SEVENDAYZ = 604_800_000
+const val SEVENDAYZ = 604_800_000
 
 @RestController
 @RequestMapping("/mymatch")
